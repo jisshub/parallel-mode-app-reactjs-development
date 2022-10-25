3d71be475879afbdec44cac2b17f1748012cef60
macmini-m1-2 <jaisonjoseph@macminim12sMini.alphionsee.in>
2022-10-25 12:00:25 +0530

    table design inprogress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Workflow from './components/Workflow';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Workflow.js
+++ src/components/Workflow.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table';
+import './Workflow.css';
 
 export default function Workflow() {
   return (
     <div>
-        <Table striped bordered>
-            <thead>
-                <tr>
-                    <th>Folder Name</th>
-                    <th>File Name</th>
-                    <th>Owner Name</th>
-                    <th>Owner Email</th>
-                </tr>
-            </thead>
-        </Table>
+        <div className='table'>
+            <Table striped bordered>
+                <thead>
+                    <tr>
+                        <th>Folder Name</th>
+                        <th>File Name</th>
+                        <th>Owner Name</th>
+                        <th>Owner Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Praveen Folder</td>
+                        <td>----</td>
+                        <td>Praveen</td>
+                        <td>Praveen@gmail.com</td>
+                    </tr>
+                </tbody>                
+            </Table>
+        </div>
     </div>
   )
 }
